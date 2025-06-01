d3c0fe4f9dfbf3011e189c21d028220e64643942
sirtech-inc <sirtech.inc@gmail.com>
2025-06-01 02:17:57 -0500

    add

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   //baseURL: "http://localhost:3012/api/",
-  baseURL: "https://automotiv-back-production.up.railway.app/",
+  baseURL: "https://automotiv-back-production.up.railway.app/api/",
 });
 
 export default api;
