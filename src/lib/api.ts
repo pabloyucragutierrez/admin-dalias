c0dd65a717b787e9b18d8e85f5cf9567771678d4
pablo gutierrez <pabloyucragutierrez@gmail.com>
2026-01-18 19:20:24 -0500

    admin dalias, integración con apis rest

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  //baseURL: "https://back.automotivdelperu.com/api/",
+  //baseURL: "https://backend-dalias.onrender.com",
 });
 
 // Interceptor para agregar el token a todas las peticiones
