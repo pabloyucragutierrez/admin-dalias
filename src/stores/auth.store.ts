a2f5be39ad682ad31270f9208df727e8f35ea4b0
pablo gutierrez <pabloyucragutierrez@gmail.com>
2026-01-25 23:06:57 -0500

    dsadasd

--- src/stores/auth.store.ts
+++ src/stores/auth.store.ts
@@ -1,4 +1,5 @@
-import type { User } from "@/interfaces";
+
+import type { User } from "@/interfaces/user.interface";
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
